464b68ef728a6e9a09056a24adae2afe65265cd3
daniel.varela <daniel.varela@edreamsodigeo.com>
2024-05-13 14:39:29 +0200

    Added cors

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 const port = 3000; // Change this to your desired port
 
 app.use(express.json());
+app.use(cors({
+  origin: 'https://team-hashing.github.io',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Define the route for /generateDemo
 app.post('/generateDemo', async (req, res) => {
@@ -12,7 +18,7 @@ app.post('/generateDemo', async (req, res) => {
     const inputText = req.body.text;
 
     // Make a request to the external server
-    const response = await axios.post('http://35.224.98.88:3000/generateDemo', {
+    const response = await axios.post('http://<an ip>:<a port>/generateDemo', {
       text: inputText,
     });
 
